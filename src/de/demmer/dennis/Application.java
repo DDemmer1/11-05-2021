cd3b59782c763056728b9c9a484657d0530d3c25
DDemmer1 <Dennis.Demmer@web.de>
2021-05-11 10:42:48 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -10,6 +10,16 @@ public class Application {
 		
 		List<Integer> list = new ArrayList<>();
 		
+		
+		list.add(58);
+		list.add(1);
+		list.add(88);
+		list.add(3);
+		list.add(1);
+		list.add(5);
+		list.add(9);
+	
+
 		ListUtil util = new ListUtil();
 		
 		util.printFrom(list, 0);
--- src/de/demmer/dennis/ListUtil.java
+++ src/de/demmer/dennis/ListUtil.java
@@ -3,15 +3,20 @@ package de.demmer.dennis;
 import java.util.List;
 
 public class ListUtil {
-	
-	
+
 	public void printFrom(List<Integer> list, int startIndex) {
-		
 
+		System.out.print("Index: " + startIndex + "- > ");
+		if (startIndex >= list.size()) {
+			return;
+		}
 
-		
-		
-		
+		System.out.println(list.get(startIndex));
+
+		startIndex = startIndex + 1;
+
+		printFrom(list, startIndex);
+
 	}
 
 }
