ab266d827c4906bd154ac7ebb0a08ebdc5a82011
DDemmer1 <Dennis.Demmer@web.de>
2021-05-11 10:08:16 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,8 +1,20 @@
 package de.demmer.dennis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 	
 	public static void main(String[] args) {
+		
+		
+		List<Integer> list = new ArrayList<>();
+		
+		ListUtil util = new ListUtil();
+		
+		util.printFrom(list, 0);
+		
+		
 		
 	}
 
---
+++ src/de/demmer/dennis/ListUtil.java
@@ -0,0 +1,17 @@
+package de.demmer.dennis;
+
+import java.util.List;
+
+public class ListUtil {
+	
+	
+	public void printFrom(List<Integer> list, int startIndex) {
+		
+
+
+		
+		
+		
+	}
+
+}
